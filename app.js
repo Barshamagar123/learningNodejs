83f9d7e65dc92a846137f7f7a373a1dfe43cb51f
Barsha Magar <barshamagar@Barshas-MacBook-Air.local>
2025-05-26 18:39:26 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require("express")
+const app = express()
+// const app=require("express")() mathi ko 2 ta line aauta line ma lekhnu laaii yeasari ni lekhinxa
+app.listen(3000, function (){
+    console.log("backend has started at port 3000")
+})
