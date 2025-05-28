0506dc1619c24c79a9dfd369739fc0d806ffb3e1
Barsha Magar <barshamagar@Mac.mshome.net>
2025-05-28 16:26:05 +0545

    maks changes

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require('express')
 const app=express()
 app.set("view engine", "ejs")//tells express js to set environment for ejs to run
 app.get("/",function(request, response){
-    response.render("home.ejs")
+    let name="barsha"
+    response.render("home.ejs",{age : 23, name:"barsha"})
     // response.send("this is home page")
 })
 app.get("/about",function(request, response){
-    response.render("about.ejs")
+
+    response.render("about.ejs",{about: "about"})
 })
+
 app.listen(3000,function(){
     console.log("backend has starated at port 3000")
 })
---
+++ database/db.js
@@ -0,0 +1,2 @@
+const {Sequelize} = require("sequelize")
+const sequelize = new sequelize() // making object form sequelize class
